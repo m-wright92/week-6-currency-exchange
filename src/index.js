0cad037e3e3c7db05fb06071a1e469a13f6262e3
Michael Wright <m.wright92@icloud.com>
2022-04-22 16:08:30 -0700

    remove suggested search bar, move back towards mvp

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './js/exchange.js';
-import {suggestions} from './js/suggestions.js';
-
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
-const suggBox = searchWrapper.querySelector(".autocom-box");
-const icon = searchWrapper.querySelector(".icon");
-let linkTag = searchWrapper.querySelector("a");
-let webLink;
-
-
 
 $(document).ready(function() {
   let body;
@@ -36,62 +26,6 @@ $(document).ready(function() {
     let userCurInput = $("#currency-input").val().toUpperCase();
     $("#convert-display").text(userNumInput * body.conversion_rates[userCurInput]);
   });
-
-inputBox.onkeyup = (e)=>{
-  // let link = document.getElementsByClassName("autocom-box");
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-    if(userData){
-        icon.onclick = ()=>{
-            webLink = `https://www.google.com/search?q=${userData}`;
-            linkTag.setAttribute("href", webLink);
-            linkTag.click();
-        }
-        emptyArray = suggestions.filter((data)=>{
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-        });
-        emptyArray = emptyArray.map((data)=>{
-            // passing return data inside li tag
-            return data = `<li>${data}</li>`;
-        });
-        searchWrapper.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = suggBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].addEventListener("click", select(this));
-            // link.addEventListener('click')
-        }
-    }else{
-        searchWrapper.classList.remove("active"); //hide autocomplete box
-    }
-}
-  /* eslint-disable no-unused-vars */
-  function select(element){
-    let selectData = element.textContent;
-    // inputBox.value = selectData;
-    selectData = inputBox.value;
-    icon.onclick = ()=>{
-      webLink = `https://www.google.com/search?q=${selectData}`;
-      linkTag.setAttribute("href", webLink);
-      linkTag.click();
-    }
-    searchWrapper.classList.remove("active");
-  }
-  /* eslint-enable no-unused-vars */
-
-  function showSuggestions(list){
-    let listData;
-    let userValue;
-    if(!list.length){
-      userValue = inputBox.value;
-      listData = `<li>${userValue}</li>`;
-    }else{
-      listData = list.join('');
-    }
-    suggBox.innerHTML = listData;
-  }
 
 });
 
