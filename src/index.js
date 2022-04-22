105618564c04885a1a2a23c0af839fc7101efa76
Michael Wright <m.wright92@icloud.com>
2022-04-22 16:55:06 -0700

    WIP finalize output of conversions

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './js/exchange.js';
+import {suggestions} from './js/suggestions.js';
+
 
 $(document).ready(function() {
   let body;
 
+  for(let i = 0; i < suggestions.length; i++) {
+    $("#currency-choice").append("<option>" + suggestions[i] + "</options");
+  }
+
   $("#get-convert").on("click", function() {
     let userNumInput = $("#num-input").val();
-    let userCurInput = $("#currency-input").val().toUpperCase();
+    let userCurInput = $("#currency-choice").val().toUpperCase();
+    let apiCurInput = userCurInput.substring(0, 3);
     let promise = ExchangeService.getRates();
     promise.then(function(response) {
       body = JSON.parse(response);
-      $("#convert-display").text(userNumInput * body.conversion_rates[userCurInput]);
+      $("#convert-display").text(userNumInput * body.conversion_rates[apiCurInput]);
       $("#get-convert").hide();
       $("#get-convert2").show();
     }, function(error) {
