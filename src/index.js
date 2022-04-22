333ba90376f6a24a10af3b764640ac8b779b1f85
Michael Wright <m.wright92@icloud.com>
2022-04-22 15:13:03 -0700

    WIP attempt to fix suggested search bar

--- src/index.js
+++ src/index.js
@@ -12,59 +12,10 @@ const icon = searchWrapper.querySelector(".icon");
 let linkTag = searchWrapper.querySelector("a");
 let webLink;
 
-inputBox.onkeyup = (e)=>{
-  let userData = e.target.value; //user enetered data
-  let emptyArray = [];
-  if(userData){
-      icon.onclick = ()=>{
-          webLink = `https://www.google.com/search?q=${userData}`;
-          linkTag.setAttribute("href", webLink);
-          linkTag.click();
-      }
-      emptyArray = suggestions.filter((data)=>{
-          return data.toLocaleLowerCase().includes(userData.toLocaleLowerCase());
-      });
-      emptyArray = emptyArray.map((data)=>{
-          return data = `<li>${data}</li>`;
-      });
-      searchWrapper.classList.add("active"); 
-      showSuggestions(emptyArray);
-      let allList = suggBox.querySelectorAll("li");
-      for (let i = 0; i < allList.length; i++) {
-          allList[i].setAttribute(onclick, select(this));
-      }
-  }else{
-      searchWrapper.classList.remove("active"); //hide autocomplete box
-  }
-}
-
-function select(element){
-  let selectData = element.textContent;
-  console.log(selectData);
-  inputBox.value = selectData;
-  icon.onclick = ()=>{
-      webLink = `https://www.google.com/search?q=${selectData}`;
-      linkTag.setAttribute("href", webLink);
-      linkTag.click();
-  }
-  searchWrapper.classList.remove("active");
-}
-
-function showSuggestions(list){
-  let listData;
-  if(!list.length){
-      let userValue = inputBox.value;
-      listData = `<li>${userValue}</li>`;
-  }else{
-    listData = list.join('');
-  }
-  suggBox.innerHTML = listData;
-}
 
 
 $(document).ready(function() {
   let body;
-  console.log(suggestions);
 
   $("#get-convert").on("click", function() {
     let userNumInput = $("#num-input").val();
@@ -85,6 +36,62 @@ $(document).ready(function() {
     let userCurInput = $("#currency-input").val().toUpperCase();
     $("#convert-display").text(userNumInput * body.conversion_rates[userCurInput]);
   });
+
+inputBox.onkeyup = (e)=>{
+  // let link = document.getElementsByClassName("autocom-box");
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = `https://www.google.com/search?q=${userData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].addEventListener("click", select(this));
+            // link.addEventListener('click')
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
+  /* eslint-disable no-unused-vars */
+  function select(element){
+    let selectData = element.textContent;
+    // inputBox.value = selectData;
+    selectData = inputBox.value;
+    icon.onclick = ()=>{
+      webLink = `https://www.google.com/search?q=${selectData}`;
+      linkTag.setAttribute("href", webLink);
+      linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+  }
+  /* eslint-enable no-unused-vars */
+
+  function showSuggestions(list){
+    let listData;
+    let userValue;
+    if(!list.length){
+      userValue = inputBox.value;
+      listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+  }
 
 });
 
