620065762c3178699c38c09d400ec9b012bfb052
Michael Wright <m.wright92@icloud.com>
2022-04-24 16:37:46 -0700

    add conditionals to catch NaN errors, add fake currency for error response test

--- src/index.js
+++ src/index.js
@@ -20,9 +20,15 @@ $(document).ready(function() {
     let promise = ExchangeService.getRates();
     promise.then(function(response) {
       body = JSON.parse(response);
-      $("#convert-display").text(userNumInput * body.conversion_rates[apiCurInput]);
-      $("#get-convert").hide();
-      $("#get-convert2").show();
+      if(isNaN(userNumInput * body.conversion_rates[apiCurInput])) {
+        $("#curr-error").text("Unfortunately that currency does not yet exist in our database, please try another currency");
+        $("#get-convert").hide();
+        $("#get-convert2").show();
+      } else {
+        $("#convert-display").text("$" + userNumInput * body.conversion_rates[apiCurInput]);
+        $("#get-convert").hide();
+        $("#get-convert2").show();
+      }
     }, function(error) {
       $("#curr-error").text(`There was an error processing your request: ${error}. Please try again`);
     });
@@ -30,8 +36,13 @@ $(document).ready(function() {
 
   $("#get-convert2").on("click", function() {
     let userNumInput = $("#num-input").val();
-    let userCurInput = $("#currency-input").val().toUpperCase();
-    $("#convert-display").text(userNumInput * body.conversion_rates[userCurInput]);
+    let userCurInput = $("#currency-choice").val().toUpperCase();
+    let curCodeInput = userCurInput.substring(0, 3);
+    if(isNaN(userNumInput * body.conversion_rates[curCodeInput])) {
+      $("#curr-error").text("Unfortunately that currency does not yet exist in our database, please try another currency");
+    } else {
+      $("#convert-display").text("$" + userNumInput * body.conversion_rates[curCodeInput]);
+    }
   });
 
 });
